73119a8ff63c5a589dec0dac6c3c099871e7b0e4
Areeb H Husain <AHHusain@bpl.netlink.com>
2025-08-25 13:35:28 +0530

    Summary : deployment

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatBot_Component from "./ChatBot_Component";
+
+const App = () => {
+  return (
+    <div>
+      <ChatBot_Component />
+    </div>
+  );
+};
+
+export default App;
